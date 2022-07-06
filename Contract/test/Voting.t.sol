a79d0e5732394793cdf18786736c30c0d9bc4a97
Clyne Fuentes <clippefuentes@gmail.com>
2022-07-06 14:22:04 -0600

    Add foundry test

---
+++ Contract/test/Voting.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@std/Test.sol";
+
+contract ContractTest is Test {
+    uint256 testNumber;
+    function setUp() public {
+        testNumber = 42;
+    }
+
+    function testExample() public {
+        uint a = 1;
+        uint b = 1;
+        assertTrue(true);
+        assertEq(a, b);
+    }
+
+    function testNumberIs42() public {
+        assertEq(testNumber, 42);
+    }
+
+    function testFailSubtract43() public {
+        testNumber -= 43;
+    }
+
+    function testCannotSubtract43() public {
+        vm.expectRevert(stdError.arithmeticError);
+        testNumber -= 43;
+    }
+
+}
